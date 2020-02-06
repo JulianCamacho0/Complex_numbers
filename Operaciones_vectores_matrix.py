bad5b7402c0cdb362197e5f652014763146f75a0
JulianCamacho0 <60012637+JulianCamacho0@users.noreply.github.com>
2020-02-06 09:47:35 -0500

    Update Operaciones_vectores_matrix.py

--- Operaciones_vectores_matrix.py
+++ Operaciones_vectores_matrix.py
@@ -5,6 +5,8 @@
 import sys
 import math
 
+#---------------------------- Operaciones complejos -----------------------------
+
 def suma(cu, cd):   
     """(list, list) -> list 
     Suma entre dos numero complejos"""
@@ -22,18 +24,17 @@ def multi(cu, cd):
     a = cu[0]*cd[0] - cu[1]*cd[1]
     b =  cu[0]*cd[1] + cu[1]*cd[0]
     r = [a,b]
-
+    
     return r
-
 
 def resta(cu, cd): 
     """(list, list) -> list 
     Resta entre dos numero complejos"""
-
+    
     a = cu[0] - cd[0]
     b = cu[1] - cd[1]
     r= [a,b]
-
+    
     return r
 
 def  cociente(cu, cd):
@@ -45,6 +46,7 @@ def  cociente(cu, cd):
     r = [a,b]
 
     return r
+
 def modulo(c):
     """(list) -> list 
     Modulo de un número complejo"""
@@ -56,10 +58,12 @@ def modulo(c):
 def conju(c):
     """(list) -> list 
     Conjugado de un número complejo"""
-
+    
     r = [c[0], c[1] * -1]
 
     return r
+
+#--------------------------- Operaciones Vectores complejos----------------------
 
 def suma_vectores(v_one, v_two):
     """(list, list) -> list
@@ -68,7 +72,7 @@ def suma_vectores(v_one, v_two):
     r = []
     for i in range(len(v_one)):
         r = r + [suma(v_one[i], v_two[i])]
-        
+
     return r
 
 def inverso_vecom (v):
@@ -81,7 +85,7 @@ def inverso_vecom (v):
         for j in i:
             f = f + [-j]
         r = r + [f]
-
+        
     return r
 
 def mult_vector_esca (c, v):
@@ -91,8 +95,10 @@ def mult_vector_esca (c, v):
     r = []
     for i in v:
         r = r + [multi(c, i)]
-
+        
     return r
+
+#--------------------- Operacion Matrices complejas --------------------------
 
 def suma_matrices(m_one, m_two):
     """(list,list) -> list
@@ -103,7 +109,7 @@ def suma_matrices(m_one, m_two):
         r = r + [suma_vectores(m_one[i], m_two[i])]
 
     return r
-        
+
 def inver_matrix(m):
     """(list) -> list
     Inversa de un matriz"""
@@ -113,7 +119,7 @@ def inver_matrix(m):
         r = r + [inverso_vecom(i)]
 
     return r
-            
+
 def matrix_escalar(c, m):
     """(list, list) -> list
     Multiplicacion de un numero complejo con una matriz compleja"""
@@ -121,7 +127,7 @@ def matrix_escalar(c, m):
     r = []
     for i in m:
         r = r + [mult_vector_esca(c,i)]
-
+        
     return r
 
 def matrix_trans(m):
@@ -132,10 +138,11 @@ def matrix_trans(m):
     r = []
     for i in range(n):
         f = []
+
         for j in range(n):
             f = f + [m[j][i]]
         r = r + [f]
-
+        
     return r
 
 def matrix_conjugada(m):
@@ -148,36 +155,55 @@ def matrix_conjugada(m):
         for j in i:
             f = f + [conju(j)]
         r = r + [f]
-
+        
     return r
 
 def matriz_adjunta(m):
-
     r = matrix_conjugada(matrix_trans(m))
     return r
+
+def producto_matrix(m,n):
+    ma = [[[0,0] for j in range(len(n[0][0]))] for i in range(len(m))]
+
+    for i in range(len(m)):
+        for j in range(len(n[0])):
+            for k in range(len(n)):
+                ma[i][j] = suma(ma[i][j], multi(m[i][k], n[k][j]))
+    return ma
+
+def accion_matrix(m,n):
+    ma = [[0,0] for j in range(len(n))]
+
+    for i in range(len(m)):
+        for j in range(len(n)):
+            for k in range(len(n)):
+                ma[i] = suma(ma[i], multi(m[i][k], n[k]))
+
+    return(ma)
     
-    
+
     
 def main():
     
     c = [1, -2]
-    
-    v_one = [[1, 2], [4, -4], [0, 1]]
+
+    v_one = [[1, 2], [4, -4]]
     v_two = [[4, 5], [3, 2], [3, 2]]
     
     m_one = [ [[1, 2] , [3, 4]] , [[-4, 3], [-1, -2]] ]
     m_two = [ [[-1, -2] , [-3, -4]] , [[4, -3], [1, 2]] ]
+    
 
-    print(suma_vectores(v_one, v_two))
-    print(inverso_vecom(v_one))
-    print(mult_vector_esca(c, v_one))
-    print(suma_matrices(m_one, m_two))
-    print(inver_matrix(m_one))
-    print(matrix_escalar(c, m_one))
-    print(matrix_trans(m_one))
-    print(matrix_conjugada(m_one))
-    print(matriz_adjunta(m_one))
-
-
+##    print(suma_vectores(v_one, v_two))
+##    print(inverso_vecom(v_one))
+##    print(mult_vector_esca(c, v_one))
+##    print(suma_matrices(m_one, m_two))
+##    print(inver_matrix(m_one))
+##    print(matrix_escalar(c, m_one))
+##    print(matrix_trans(m_one))
+##    print(matrix_conjugada(m_one))
+##    print(matriz_adjunta(m_one))
+##    print(producto_matrix(m_one, m_two))
+    print(accion_matrix(m_one, v_one))
 
 main()
